e08bff38ba37c6633dc20f472c04f4630a175b11
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-08-13 15:11:05 -0300

    Ending single product page

---
+++ src/components/Price.jsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+function Price({ price, id, options }) {
+  const [total, setTotal] = useState(price)
+  const [quantity, setQuantity] = useState(1)
+  const [selected, setSelected] = useState(0)
+
+  useEffect(() => {
+    setTotal(quantity * (price + options[selected].additionalPrice))
+  }, [quantity, selected, options, price])
+
+  return (
+    <div className='flex flex-col gap-4'>
+      <h2 className='text-2xl font-bold'>{total.toFixed(2)}</h2>
+      {/* OPTIONS CONTAINER */}
+      <div className='flex gap-4'>
+        {options.map((option, index) => (
+          <button
+            className='min-w-[6rem]  p-2 ring-1 ring-red-400 rounded-md'
+            key={option.title}
+            style={{
+              background: selected === index ? 'rgb(248 113 113)' : 'white',
+              color: selected === index ? 'white' : 'red'
+            }}
+            onClick={() => setSelected(index)}>
+            {option.title}
+          </button>
+        ))}
+      </div>
+      {/* QUANTITY AND ADD BUTTON CONTAINER */}
+      <div className='flex justify-between items-center'>
+        {/* QUANTITY */}
+        <div className='flex justify-between w-full p-3 ring-1 ring-red-500'>
+          <span>Quantity</span>
+          <div className='flex gap-4 items-center'>
+            <button onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}>-</button>
+            <span>{quantity}</span>
+            <button onClick={() => setQuantity((prev) => (prev < 9 ? prev + 1 : 9))}>+</button>
+          </div>
+        </div>
+        {/* CART BUTTON */}
+        <div>
+          <button className='uppercase w-56 text-white bg-red-500 p-3 ring-1 ring-red-500'>
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Price
--- src/data.js
+++ src/data.js
@@ -348,3 +348,25 @@ export const pizzas = [
     ]
   }
 ]
+
+export const singleProduct = {
+  id: 1,
+  title: 'Sicilian',
+  desc: 'Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.',
+  img: '/temporary/p1.png',
+  price: 24.9,
+  options: [
+    {
+      title: 'Small',
+      additionalPrice: 0
+    },
+    {
+      title: 'Medium',
+      additionalPrice: 4
+    },
+    {
+      title: 'Large',
+      additionalPrice: 6
+    }
+  ]
+}
