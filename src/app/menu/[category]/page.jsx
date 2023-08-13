e3b5e7fc43e967959a45b11ecb33bdfca5626f79
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-08-13 11:56:57 -0300

    Ending category page

--- src/app/menu/[category]/page.jsx
+++ src/app/menu/[category]/page.jsx
@@ -1,5 +1,36 @@
+import { pizzas } from '@/data'
+import Image from 'next/image'
+import Link from 'next/link'
+
 function CategoryPage() {
-  return <div>CategoryPage</div>
+  return (
+    <div className='flex flex-wrap text-red-500'>
+      {pizzas.map((item) => (
+        <Link
+          key={item.id}
+          href={`/product/${item.id}`}
+          className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group even:bg-fuchsia-50'>
+          {/* IMAGE CONTAINER */}
+          <div className='relative h-[80%]'>
+            <Image
+              src={item.img}
+              alt={item.title}
+              fill
+              className='object-contain'
+            />
+          </div>
+          {/* TEXT CONTAINER */}
+          <div className='flex items-center justify-between font-bold'>
+            <h1 className='text-2xl uppercase p-2'>{item.title}</h1>
+            <h2 className='group-hover:hidden text-xl'>$ {item.price}</h2>
+            <button className='hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md'>
+              Add to Cart
+            </button>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
 }
 
 export default CategoryPage
--- src/data.js
+++ src/data.js
@@ -219,3 +219,132 @@ export const menu = [
     bg: 'white'
   }
 ]
+
+export const pizzas = [
+  {
+    id: 1,
+    title: 'Sicilian',
+    desc: 'Ignite your taste buds with a fiery combination of spicy pepperoni, jalapeños, crushed red pepper flakes, and melted mozzarella cheese, delivering a kick with every bite.',
+    img: '/temporary/p1.png',
+    price: 24.9,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 4
+      },
+      {
+        title: 'Large',
+        additionalPrice: 6
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Mediterranean Delight',
+    desc: 'Embark on a culinary journey with this Mediterranean-inspired creation, featuring zesty feta cheese, Kalamata olives, sun-dried tomatoes, and a sprinkle of oregano.',
+    img: '/temporary/p8.png',
+    price: 32.9,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 4
+      },
+      {
+        title: 'Large',
+        additionalPrice: 6
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Bella Napoli',
+    desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
+    img: '/temporary/p3.png',
+    price: 26.9,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 4
+      },
+      {
+        title: 'Large',
+        additionalPrice: 6
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: 'Pesto Primavera',
+    desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
+    img: '/temporary/p10.png',
+    price: 28.9,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 4
+      },
+      {
+        title: 'Large',
+        additionalPrice: 6
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: 'Veggie Supreme',
+    desc: 'A classic Italian delight featuring a thin, crispy crust, tangy tomato sauce, fresh mozzarella, and a medley of aromatic herbs topped with lettuce, tomatoes, and a dollop of tangy mayo.',
+    img: '/temporary/p11.png',
+    price: 24.9,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 4
+      },
+      {
+        title: 'Large',
+        additionalPrice: 6
+      }
+    ]
+  },
+  {
+    id: 6,
+    title: 'Four Cheese Fantasy',
+    desc: 'Experience pure cheesy bliss with a melty blend of mozzarella, cheddar, provolone, and Parmesan cheeses, creating a rich and indulgent pizza experience.',
+    img: '/temporary/p12.png',
+    price: 22.9,
+    options: [
+      {
+        title: 'Small',
+        additionalPrice: 0
+      },
+      {
+        title: 'Medium',
+        additionalPrice: 4
+      },
+      {
+        title: 'Large',
+        additionalPrice: 6
+      }
+    ]
+  }
+]
