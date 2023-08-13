bd75a144da8e5f753189da57dd001467312112c5
Stefania Veronica Bruera <59149@sistemas.frc.utn.edu.ar>
2023-08-13 20:40:18 -0300

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
+    <div className='h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between'>
       <Link
         href='/'
         className='font-bold text-xl'>
